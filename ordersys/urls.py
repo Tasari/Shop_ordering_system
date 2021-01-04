ec860b3b523eb0cb48e33da17c8e73a961c0f2ab
Tasari <Tasari@wp.pl>
2021-01-04 07:08:29 +0100

    Made urls more RESTful

--- ordersys/urls.py
+++ ordersys/urls.py
@@ -5,14 +5,15 @@ from . import views
 app_name = 'ordersys'
 
 urlpatterns = [
-    path('', views.OrdersView.as_view(), name='index'),
-    path('pending/', views.PendingOrdersView.as_view(), name='pending'),
-    path('<int:pk>/start_preparing/', views.start_preparing_order, name='start_preparing'),
-    path('<int:pk>/finish_preparing/', views.finish_preparing_order, name='finish_preparing'),
-    path('<int:pk>/collect_order/', views.collected_order, name='collect_preparing'),
-    path('prepare/', views.PrepareOrdersView.as_view(), name='prepare'),
-    path('collect/', views.CollectOrdersView.as_view(), name='collect'),
-    path('done/', views.DoneOrdersView.as_view(), name='done'),
-    path('failed/', views.FailedOrdersView.as_view(), name='failed'),
-    path('customers/', views.CustomersOrdersView.as_view(), name='customers'),
+    path('orders/', views.OrdersView.as_view(), name='index'),
+    path('orders/pending/', views.PendingOrdersView.as_view(), name='pending'),
+    path('orders/<int:pk>/start_preparing/', views.start_preparing_order, name='start_preparing'),
+    path('orders/<int:pk>/finish_preparing/', views.finish_preparing_order, name='finish_preparing'),
+    path('orders/<int:pk>/collect_order/', views.collected_order, name='collect_preparing'),
+    path('orders/prepare/', views.PrepareOrdersView.as_view(), name='prepare'),
+    path('orders/collect/', views.CollectOrdersView.as_view(), name='collect'),
+    path('orders/done/', views.DoneOrdersView.as_view(), name='done'),
+    path('orders/failed/', views.FailedOrdersView.as_view(), name='failed'),
+    path('orders/customers/', views.CustomersOrdersView.as_view(), name='customers'),
+    path('orders/create/', views.CreateOrderView.as_view())
 ]
