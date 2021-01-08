52e97dd973c61c802e85d97021df8a04ebeaa53b
Tasari <Tasari@wp.pl>
2021-01-08 09:16:31 +0100

    Created forms file

---
+++ ordersys/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+
+from .models import Product, TempOrder, ProductAmount
+
+class ProductAmountForm(forms.ModelForm):
+    product = forms.ModelChoiceField(queryset=Product.objects.all(), empty_label='None', required=False)
+    amount = forms.IntegerField(required=False)
+    to_delete = forms.ModelChoiceField(queryset=Product.objects.filter(id__in=TempOrder.objects.values('product')), empty_label='None', required=False)
+    class Meta:
+        model = ProductAmount
+        fields = ('product', 'amount')
