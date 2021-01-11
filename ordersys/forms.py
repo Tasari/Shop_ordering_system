cfc41510abfd3df8b53239e140829ef35b2791d8
Tasari <Tasari@wp.pl>
2021-01-11 07:30:43 +0100

    Created Login Form

--- ordersys/forms.py
+++ ordersys/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.models import User
 
 from .models import Product, TempOrder, ProductAmount
 
@@ -9,3 +10,10 @@ class OrderCreationForm(forms.ModelForm):
     class Meta:
         model = ProductAmount
         fields = ('product', 'amount')
+
+class LogInForm(forms.ModelForm):
+    username = forms.CharField()
+    password = forms.PasswordInput()
+    class Meta:
+        model = User
+        fields = ('username', 'password')
