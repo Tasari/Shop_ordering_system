ca678e153958270c122dca5d4cdb094f14f1493b
Tasari <Tasari@wp.pl>
2021-01-14 09:23:36 +0100

    Added product adding test

--- ordersys/tests.py
+++ ordersys/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from django.shortcuts import reverse
 
-# Create your tests here.
+from ordersys.models import Product
+
+class TestOrderCreationView(TestCase):
+    def test_adding_product_to_temp_ord(self):
+        user = User.objects.create(username='test')
+        user.set_password('12345')
+        user.save()
+
+        product = Product(name='Test')
+        product.save()
+        product = Product.objects.last()
+        c = Client()
+
+        response = c.post(reverse('ordersys:login'), {'username':'test', 'password':'12345'}, follow=True)
+        response = c.post(reverse('ordersys:create'), {'product':product.id, 'amount':3, 'to_delete':'', 'Add':True})
+        response = c.get('/ordersys/orders/create/')
+        self.assertContains(response, "Test: 3")
+
